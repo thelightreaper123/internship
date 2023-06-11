bfdd7860a6fb907fc33840aaf9610f95ae10bff4
thelightreaper123 <oeyitade@gmail.com>
2023-06-11 21:47:47 +0100

    modified

--- week3project/main.js
+++ week3project/main.js
@@ -14,16 +14,13 @@ backarrow.addEventListener("click", function(){
 
 inputfield.addEventListener("keyup", function(e){
 if(e.key === "Enter" && inputfield.value != ""){
-    requestApi(inputfield.value);       
+    requestApi(inputfield.value); 
+    setTimeout(() => {
+        var paragraph = document.createElement("p");
+        paragraph.innerText = inputfield.value;
+         searchhistory.appendChild(paragraph);
+  }, 3000);      
 }
-setTimeout(() => {
-  if(e.key === "Enter" && inputfield.value != ""){
-       var paragraph = document.createElement("p");
-       paragraph.innerText = inputfield.value;
-        searchhistory.appendChild(paragraph);
-        
-    }
- }, 3000);
 });
 
 var apiKey = 'a9bc860599772b42085366737fb5388d';
@@ -34,9 +31,14 @@ function requestApi(city){
     fetchdata();
 }
 
-function fetchdata(){
+async function fetchdata(){
     
-    fetch(api).then(response => response.json()).then(result => weatherDetails(result));
+    try{
+        const response = await fetch(api).then(response => response.json()).then(result => weatherDetails(result));
+    } catch(error){
+        alert("error fetching data refresh page");
+    }
+    
 }
 
 function weatherDetails(info){
@@ -53,20 +55,20 @@ function weatherDetails(info){
         if(id == 800){
         icons.src = "images/clear.png"
         } else if(id >= 200 && id<= 232){
-            icons.src = "images/storm.svg"
-            }else if(id >= 600 && id<= 622){
-                icons.src = "images/snow.png"
-                }else if(id >= 701 && id<= 781){
-                    icons.src = "images/mist.png"
-                    } else if(id >= 801 && id<= 804){
-                        icons.src = "images/clouds.png"
-                        }else if(id >= 300 && id<= 321){
-                            icons.src = "images/drizzle.png"
-                            }else if(id >= 500 && id<= 521){
-                                icons.src = "images/rain.png"
-                                }
+        icons.src = "images/storm.svg"
+        }else if(id >= 600 && id<= 622){
+        icons.src = "images/snow.png"
+       }else if(id >= 701 && id<= 781){
+       icons.src = "images/mist.png"
+        } else if(id >= 801 && id<= 804){
+         icons.src = "images/clouds.png"
+        }else if(id >= 300 && id<= 321){
+       icons.src = "images/drizzle.png"
+       }else if(id >= 500 && id<= 521){
+       icons.src = "images/rain.png"
+       }
 
-        wrapper.querySelector(".temp .num").innerText = Math.floor(temp);
+        wrapper.querySelector(".temp .num").innerText = temp;
         wrapper.querySelector(".weather").innerText = description;
         wrapper.querySelector(".location span").innerText = `${city}, ${country}`;
         wrapper.querySelector(".temp .num-2").innerText = Math.floor(feels_like);
